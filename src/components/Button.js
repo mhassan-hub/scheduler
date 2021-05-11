eaf86abc553f91d93874101f4e00afea6a9eb55a
Mohamed Hassan <mhass405@gmail.com>
2021-05-11 01:19:08 -0400

    Create a dynamic button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,39 @@
 import React from "react";
-
+import classNames from 'classnames';
 import "components/Button.scss";
 
+// MY WAY
 export default function Button(props) {
-   return <></>;
-}
+   
+   return (
+   <button 
+      className={classNames("button", " button--danger", " button--confirm")}
+      onClick={props.onClick}
+      disabled={props.disabled}
+   >
+      {props.children}
+      </button>
+   );
+ }
+
+
+ /* COMPASS WAY
+ 
+ export default function Button(props) {
+  const buttonClass = classnames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+} 
+ 
+ */
