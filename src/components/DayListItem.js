e16ce8a9711fee9e652a77fb4cc8f78eaf4cf1b9
Mohamed Hassan <mhass405@gmail.com>
2021-05-11 03:07:52 -0400

    Add day list to aplication component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,16 +6,16 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   const {name, spots, selected} = props;
   const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0
   });
   const formatSpots = () => {
     return (!spots ? "no spots remaining" : (spots === 1 ? `1 spot remaining` : `${spots} spots remaining`));
   }
 
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className={dayClass}>{name}</h2> 
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
